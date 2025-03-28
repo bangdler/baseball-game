20bdd155d2f7bb163c1d8fcae6575695818bb1e9
bangdler <zbthz90@gmail.com>
2025-03-28 14:13:53 +0900

    refact: BaseBallNumber 유효성 검사 시 에러 메세지 분기

--- src/domain/BaseBallNumber.ts
+++ src/domain/BaseBallNumber.ts
@@ -4,29 +4,36 @@ export default class BaseBallNumber {
   numbers: TBaseBallNumber;
 
   constructor(input: string) {
-    if (this.valitdate(input)) {
+    const validateResult = this.valitdate(input);
+    if (validateResult.result) {
       this.numbers = input.split("").map(Number) as TBaseBallNumber;
     } else {
-      throw new Error("유효하지 않은 입력입니다.");
+      throw new Error(validateResult.message);
     }
   }
 
-  valitdate(input: string) {
+  valitdate(
+    input: string
+  ): { result: true } | { result: false; message: string } {
     let validate = true;
+    let message = "";
     const set = new Set(input.split(""));
     // 길이가 3개인지 확인
     if (input.length !== 3) {
       validate = false;
+      message = "세자리로 입력해주세요.";
     }
     // 숫자인지 확인
     else if (Number.isNaN(Number(input))) {
       validate = false;
+      message = "숫자로 입력해주세요.";
     }
     // 중복 확인
     else if (set.size !== 3) {
       validate = false;
+      message = "중복 없이 입력해주세요.";
     }
 
-    return validate;
+    return validate ? { result: true } : { result: false, message };
   }
 }
