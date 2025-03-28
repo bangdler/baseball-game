aab93f7c3b212a3ba5bafe8a492e5340d9cb3d36
bangdler <zbthz90@gmail.com>
2025-03-28 17:29:56 +0900

    refact: RandomBallCreator 타입 재수정

--- src/domain/RandomBallCreator.ts
+++ src/domain/RandomBallCreator.ts
@@ -1,7 +1,7 @@
 import { TBaseBallNumber } from "./BaseBallNumber";
 
 export default class RandomBallCreator {
-  static createRandomBalls(): TBaseBallNumber {
+  static createRandomBalls(): number[] {
     const ballNumbers: number[] = [];
     while (ballNumbers.length < 3) {
       const randomNumber = Math.floor(Math.random() * 9) + 1;
@@ -9,6 +9,6 @@ export default class RandomBallCreator {
         ballNumbers.push(randomNumber);
       }
     }
-    return ballNumbers as TBaseBallNumber;
+    return ballNumbers;
   }
 }
