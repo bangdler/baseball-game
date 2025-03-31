4d9aafccf35902b0ca9f62755128e2b5a5c8c16d
bangdler <zbthz90@gmail.com>
2025-03-31 15:03:01 +0900

    refact: 미사용 hook 주석처리

--- src/hooks/useBaseballGame.tsx
+++ src/hooks/useBaseballGame.tsx
@@ -1,26 +1,26 @@
 import { useRecoilState } from "recoil";
 import { BaseBallGameStore } from "../state/RecoilSampleState";
 
-export const useBaseballGame = () => {
-  const [baseballState, setBaseballState] = useRecoilState(BaseBallGameStore);
-  console.log(...baseballState.game.answer.numbers);
+// export const useBaseballGame = () => {
+//   const [baseballState, setBaseballState] = useRecoilState(BaseBallGameStore);
+//   console.log(...baseballState.game.answer.numbers);
 
-  const checkAndAddHistory = (input: string) => {
-    setBaseballState({
-      game: baseballState.game.run(input),
-    });
-  };
+//   const checkAndAddHistory = (input: string) => {
+//     setBaseballState({
+//       game: baseballState.game.run(input),
+//     });
+//   };
 
-  const resetGame = () => {
-    setBaseballState({
-      game: baseballState.game.reset(),
-    });
-  };
+//   const resetGame = () => {
+//     setBaseballState({
+//       game: baseballState.game.reset(),
+//     });
+//   };
 
-  return {
-    checkAndAddHistory,
-    resetGame,
-    history: baseballState.game.history,
-    isEnd: baseballState.game.isEnd(),
-  };
-};
+//   return {
+//     checkAndAddHistory,
+//     resetGame,
+//     history: baseballState.game.history,
+//     isEnd: baseballState.game.isEnd(),
+//   };
+// };
