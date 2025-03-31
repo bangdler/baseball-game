dccf6ebf37466bbd80509229af8b29364d422781
bangdler <zbthz90@gmail.com>
2025-03-31 15:03:38 +0900

    feat: 참여자 목록 UI용 util 구현

--- src/utils/BaseballGameUtils.ts
+++ src/utils/BaseballGameUtils.ts
@@ -1,20 +1,35 @@
-import { IHistoryItem } from './../domain/BaseballGame';
+import BaseballGame, { IHistoryItem } from "./../domain/BaseballGame";
 
 export class BaseballGameUtils {
-    static makeGameResult(result: IHistoryItem) {
-        const { input, strike, ball } = result;
+  static makeGameResult(result: IHistoryItem) {
+    const { input, strike, ball } = result;
 
-        const inputString = input.join("");
-        let resultString;
+    const inputString = input.join("");
+    let resultString;
 
-        if (strike === 3) {
-            resultString = `${inputString} : 정답입니다!`;
-        } else if (strike === 0 && ball === 0) {
-            resultString = `${inputString} : 낫싱`;
-        } else {
-            resultString = `${inputString} : ${strike} 스트라이크 ${ball} 볼`;
-        }
-
-        return resultString;
+    if (strike === 3) {
+      resultString = `${inputString} : 정답입니다!`;
+    } else if (strike === 0 && ball === 0) {
+      resultString = `${inputString} : 낫싱`;
+    } else {
+      resultString = `${inputString} : ${strike} 스트라이크 ${ball} 볼`;
     }
+
+    return resultString;
+  }
+
+  static makeGamesInfo(baseballGames: BaseballGame[]) {
+    if (baseballGames.length === 0) {
+      return "참여중인 사용자가 없습니다.";
+    }
+    let info = "참여중인 사용자 목록:\n";
+    baseballGames.forEach((game, idx) => {
+      if (idx === baseballGames.length - 1) {
+        info += `${game.username}`;
+      } else {
+        info += `${game.username}, `;
+      }
+    });
+    return info;
+  }
 }
