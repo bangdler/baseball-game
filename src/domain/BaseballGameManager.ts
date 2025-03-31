17d81f59e340c6886fa4404e8611d0a2a684f836
bangdler <zbthz90@gmail.com>
2025-03-31 17:47:54 +0900

    feat: 현재 활성화된 게임 인덱스 추가 및 관리

--- src/domain/BaseballGameManager.ts
+++ src/domain/BaseballGameManager.ts
@@ -6,6 +6,7 @@ export default class BaseballGameManager {
   maxUserCount: number;
   baseballGames: BaseballGame[];
   commonAnswer: BaseBallNumber;
+  currentGameIdx: number;
 
   constructor({
     maxUserCount,
@@ -13,14 +14,17 @@ export default class BaseballGameManager {
     commonAnswer = new BaseBallNumber(
       RandomBallCreator.createRandomBalls().join("")
     ),
+    currentGameIdx = 0,
   }: {
     maxUserCount: number;
     baseballGames?: BaseballGame[];
     commonAnswer?: BaseBallNumber;
+    currentGameIdx?: number;
   }) {
     this.maxUserCount = maxUserCount;
     this.baseballGames = baseballGames;
     this.commonAnswer = commonAnswer;
+    this.currentGameIdx = currentGameIdx;
   }
 
   addBaseballGame(username: string): BaseballGameManager {
@@ -38,10 +42,11 @@ export default class BaseballGameManager {
       maxUserCount: this.maxUserCount,
       baseballGames: newBaseBallGames,
       commonAnswer: this.commonAnswer,
+      currentGameIdx: this.currentGameIdx,
     });
   }
 
-  runGame(id: string, input: string) {
+  runGame(id: string, input: string): BaseballGameManager {
     const baseballGame = this.baseballGames.find((game) => game.id === id);
     if (!baseballGame) {
       throw new Error("게임을 찾을 수 없습니다.");
@@ -51,23 +56,28 @@ export default class BaseballGameManager {
     const newBaseballGames = this.baseballGames.map((game) =>
       game.id === id ? newBaseBallGame : game
     );
+    const nextGameIdx = this.getNextIdx(
+      this.currentGameIdx,
+      newBaseballGames.length
+    );
 
     return new BaseballGameManager({
       maxUserCount: this.maxUserCount,
       baseballGames: newBaseballGames,
       commonAnswer: this.commonAnswer,
+      currentGameIdx: nextGameIdx,
     });
   }
 
-  isEnd() {
+  isEnd(): boolean {
     return this.baseballGames.some((game) => game.isEnd());
   }
 
-  isMaxUserCount() {
+  isMaxUserCount(): boolean {
     return this.baseballGames.length >= this.maxUserCount;
   }
 
-  reset() {
+  reset(): BaseballGameManager {
     const newCommonAnswer = new BaseBallNumber(
       RandomBallCreator.createRandomBalls().join("")
     );
@@ -85,7 +95,17 @@ export default class BaseballGameManager {
     });
   }
 
-  removeBaseballGame(id: string) {
+  removeBaseballGame(id: string): BaseballGameManager {
+    const removeIdx = this.baseballGames.findIndex((game) => game.id === id);
+    let reconciledIdx = this.currentGameIdx;
+    if (removeIdx !== -1) {
+      reconciledIdx = this.getReconciledIdx(
+        removeIdx,
+        this.currentGameIdx,
+        this.baseballGames.length
+      );
+    }
+
     const newBaseballGames = [...this.baseballGames].filter(
       (game) => game.id !== id
     );
@@ -94,6 +114,39 @@ export default class BaseballGameManager {
       maxUserCount: this.maxUserCount,
       baseballGames: newBaseballGames,
       commonAnswer: this.commonAnswer,
+      currentGameIdx: reconciledIdx,
     });
+  }
+
+  isCurrentGame(id: string): boolean {
+    const gameIdx = this.baseballGames.findIndex((game) => game.id === id);
+    if (gameIdx === -1) {
+      throw new Error("게임을 찾을 수 없습니다.");
+    }
+    return gameIdx === this.currentGameIdx;
+  }
+
+  getNextIdx(curIdx: number, length: number): number {
+    const nextIdx = curIdx + 1;
+    if (nextIdx >= length) {
+      return 0;
+    }
+    return nextIdx;
+  }
+
+  getPrevIdx(curIdx: number, length: number): number {
+    const prevIdx = curIdx - 1;
+    if (prevIdx < 0) {
+      return length - 1;
+    }
+    return prevIdx;
+  }
+
+  getReconciledIdx(removeIdx: number, curIdx: number, length: number): number {
+    let reconciledIdx = curIdx;
+    if (removeIdx < curIdx) {
+      reconciledIdx = this.getPrevIdx(curIdx, length);
+    }
+    return reconciledIdx;
   }
 }
