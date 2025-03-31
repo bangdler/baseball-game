a7e945325214b98a4ced93c4769a87790f9cc4d5
bangdler <zbthz90@gmail.com>
2025-03-31 17:48:17 +0900

    test: 게임인덱스 관련 테스트 코드 추가

--- src/domain/BaseballGameManager.spec.ts
+++ src/domain/BaseballGameManager.spec.ts
@@ -79,4 +79,52 @@ describe("BaseballGameManager", () => {
     expect(gameManager.baseballGames.length).toBe(1);
     expect(gameManager.baseballGames[0].username).toBe("user2");
   });
+
+  it("reset 메서드 - 게임 초기화 확인", () => {
+    gameManager = gameManager.addBaseballGame("user1").addBaseballGame("user2");
+    gameManager = gameManager
+      .runGame(gameManager.baseballGames[0].id, "123")
+      .runGame(gameManager.baseballGames[1].id, "456");
+    expect(gameManager.baseballGames[0].history.length).toBe(1);
+    expect(gameManager.baseballGames[1].history.length).toBe(1);
+    expect(gameManager.baseballGames[0].state).toBe("END");
+    const resetGameManager = gameManager.reset();
+    expect(resetGameManager.baseballGames[0].history.length).toBe(0);
+    expect(resetGameManager.baseballGames[0].state).toBe("PLAYING");
+    expect(resetGameManager.baseballGames[1].history.length).toBe(0);
+  });
+
+  it("isCurrentGame 메서드 - 현재 게임 확인", () => {
+    gameManager = gameManager.addBaseballGame("user1").addBaseballGame("user2");
+    expect(gameManager.isCurrentGame(gameManager.baseballGames[0].id)).toBe(
+      true
+    );
+    expect(gameManager.isCurrentGame(gameManager.baseballGames[1].id)).toBe(
+      false
+    );
+
+    gameManager = gameManager.runGame(gameManager.baseballGames[0].id, "135");
+    expect(gameManager.isCurrentGame(gameManager.baseballGames[0].id)).toBe(
+      false
+    );
+    expect(gameManager.isCurrentGame(gameManager.baseballGames[1].id)).toBe(
+      true
+    );
+  });
+
+  it("getNextIdx 메서드 - 다음 인덱스 확인", () => {
+    expect(gameManager.getNextIdx(0, 2)).toBe(1);
+    expect(gameManager.getNextIdx(1, 2)).toBe(0);
+  });
+
+  it("getPrevIdx 메서드 - 이전 인덱스 확인", () => {
+    expect(gameManager.getPrevIdx(0, 2)).toBe(1);
+    expect(gameManager.getPrevIdx(1, 2)).toBe(0);
+  });
+
+  it("getReconciledIdx 메서드 - 인덱스 조정 확인", () => {
+    expect(gameManager.getReconciledIdx(0, 3, 5)).toBe(2);
+    expect(gameManager.getReconciledIdx(3, 3, 5)).toBe(3);
+    expect(gameManager.getReconciledIdx(4, 3, 5)).toBe(3);
+  });
 });
