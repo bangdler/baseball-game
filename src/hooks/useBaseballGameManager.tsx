58909044b0434d18058e36a06b5e240ec766f177
bangdler <zbthz90@gmail.com>
2025-03-31 14:54:59 +0900

    feat: gameManager 관련 상태 및 hook 추가

---
+++ src/hooks/useBaseballGameManager.tsx
@@ -0,0 +1,43 @@
+import { useRecoilState } from "recoil";
+import { BaseBallGameStore } from "../state/RecoilSampleState";
+
+export const useBaseballGameManager = () => {
+  const [baseballState, setBaseballState] = useRecoilState(BaseBallGameStore);
+  console.log(...baseballState.gameManager.commonAnswer.numbers);
+
+  const addBaseballGame = (username: string) => {
+    setBaseballState({
+      gameManager: baseballState.gameManager.addBaseballGame(username),
+    });
+  };
+
+  const checkAndAddHistory = (id: string, input: string) => {
+    setBaseballState({
+      gameManager: baseballState.gameManager.runGame(id, input),
+    });
+  };
+
+  const resetGame = () => {
+    setBaseballState({
+      gameManager: baseballState.gameManager.reset(),
+    });
+  };
+
+  const removeBaseballGame = (id: string) => {
+    setBaseballState({
+      gameManager: baseballState.gameManager.removeBaseballGame(id),
+    });
+  };
+
+  return {
+    addBaseballGame,
+    removeBaseballGame,
+    checkAndAddHistory,
+    resetGame,
+    baseballGames: baseballState.gameManager.baseballGames,
+    isEnd: baseballState.gameManager.isEnd(),
+    isMaxUserCount:
+      baseballState.gameManager.baseballGames.length >=
+      baseballState.gameManager.maxUserCount,
+  };
+};
--- src/state/RecoilSampleState.ts
+++ src/state/RecoilSampleState.ts
@@ -1,19 +1,16 @@
 import { atom } from "recoil";
-import BaseballGame from "../domain/BaseballGame";
-import RandomBallCreator from "../domain/RandomBallCreator";
-import BaseBallNumber from "../domain/BaseBallNumber";
+
+import BaseballGameManager from "../domain/BaseballGameManager";
 
 type State = {
-  game: BaseballGame;
+  gameManager: BaseballGameManager;
 };
 
 export const BaseBallGameStore = atom<State>({
   key: "baseBallGame",
   default: {
-    game: new BaseballGame({
-      answer: new BaseBallNumber(
-        RandomBallCreator.createRandomBalls().join("")
-      ),
+    gameManager: new BaseballGameManager({
+      maxUserCount: 3,
     }),
   },
 });
