d9776c51a2731eb403e4d28164db2c19bc150d3c
bangdler <zbthz90@gmail.com>
2025-04-02 13:40:05 +0900

    refact: zustand 초기상태 반영

--- src/state/ZustandSampleState.ts
+++ src/state/ZustandSampleState.ts
@@ -1,12 +1,16 @@
 import { create } from "zustand/react";
-import BaseballGame from "../domain/BaseballGame";
+import { BaseballGame } from "../domain/BaseballGame";
+import BaseBallNumber from "../domain/BaseBallNumber";
+import RandomBallCreator from "../domain/RandomBallCreator";
 
 interface State {
   game: BaseballGame;
 }
-
+  
 interface Actions {}
 
 export const sampleZustandStore = create<State & Actions>((set) => ({
-  game: new BaseballGame({}),
+  game: new BaseballGame({
+    answer: new BaseBallNumber(RandomBallCreator.createRandomBalls().join("")),
+  }),
 }));
